65943181981de9b91c1406b61ad2d0997bc07111
Ip-ard <uachmadafif@gmail.com>
2023-07-10 13:16:24 +0700

    okay

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:laundry/src/pages/Mitra/Dashboard/index.dart';
 import 'package:laundry/src/pages/Register/index.dart';
 import 'package:laundry/src/pages/splashscreen.dart';
 import 'package:laundry/src/pages/User/Dashboard/index.dart';
@@ -10,4 +11,5 @@ final List<GetPage<dynamic>>? routes = [
   GetPage(name: registerRoute, page: () => Register()),
   GetPage(name: loginRoute, page: () => LoginPage()),
   GetPage(name: userRoute, page: () => UserDashboard()),
+  GetPage(name: mitraRoute, page: () => MitraDashboard()),
 ];
--- lib/src/services/assets.dart
+++ lib/src/services/assets.dart
@@ -1,1 +1,2 @@
 const String logo = 'assets/logo.png';
+const String user = 'assets/user.png';
